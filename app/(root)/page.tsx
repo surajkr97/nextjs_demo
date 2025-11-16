0b73be49ceddd1be0bd7ce9395a299e8a10f6e0f
Suraj Kumar <surajkumar06174@gmail.com>
2025-11-16 14:50:12 +0530

    Implement album fetching in Home component by creating app/route.ts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,24 @@
-import Hello from "../../components/hello";
+async function Home() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+  if (!response.ok) throw new Error("Failed to fetch data");
 
-const Home = () => {
+  const albums = await response.json();
+
   return (
-    <>
-      <Hello />
-      Welcome to Next.js!
-    </>
-  )
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4">
+      {albums.map((album: { id: number; title: string }) => (
+        <div
+          key={album.id}
+          className="bg-orange-50 shadow-md rounded-lg p-4 border border-gray-200 hover:shadow-lg transition"
+        >
+          <h3 className="text-lg font-bold mb-2 text-gray-100">
+            {album.title}
+          </h3>
+          <p className="text-gray-400">Album ID: {album.id}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
